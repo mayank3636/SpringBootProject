af1a51730afdb81daa1164a4ea9572058846379c
Bhavana Thimmappa <M1032479@mindtree.com>
2017-03-18 18:23:28 +0530

    New Code commit Spring boot project

---
+++ src/main/java/com/mindtree/skillspeed/hotelmanagement/dao/CityEntityDAO.java
@@ -0,0 +1,9 @@
+package com.mindtree.skillspeed.hotelmanagement.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mindtree.skillspeed.hotelmanagement.entity.CityEntity;
+
+public interface CityEntityDAO extends CrudRepository<CityEntity, Integer> {
+
+}
